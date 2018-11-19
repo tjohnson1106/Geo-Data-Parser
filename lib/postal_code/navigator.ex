d395fa045de94c183463bccb51bb61a66a2e0685
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-19 15:44:18 -0500

    implemented store alias

--- lib/postal_code/navigator.ex
+++ lib/postal_code/navigator.ex
@@ -1,4 +1,6 @@
 defmodule ElhexDelivery.PostalCode.Navigator do
+  alias ElhexDelivery.PostalCode.Store
+
   def get_distance(from, to) do
     do_get_distance(from, to)
   end
@@ -7,11 +9,13 @@ defmodule ElhexDelivery.PostalCode.Navigator do
   # ^public method convention dictates it's prefixed with 'do'
   # def foo_bar defp defp do_foo_bar
 
+  # calculate_distance should remain static, at least for now
+
   defp do_get_distance(from, to) do
     {lat1, long1} = get_geolocation(from)
     {lat2, long2} = get_geolocation(to)
 
-    22.95
+    calculate_distance({lat1, long1}, {lat2, long2})
   end
 
   defp get_geolocation(postal_code) when is_binary(postal_code) do
